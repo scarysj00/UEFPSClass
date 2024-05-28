423c29e513d62ec72fb6ed672b82b6321ce8670f
scarysj00 <juliannek08@gmail.com>
2024-05-28 21:07:52 +0900

    C++파일 생성, IMC,IA 설정

---
+++ FPSTank/Intermediate/Build/Win64/UnrealEditor/Inc/FPSTank/UHT/PUCGameModeBase.generated.h
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Actors/GameMode/PUCGameModeBase.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FPSTANK_PUCGameModeBase_generated_h
+#error "PUCGameModeBase.generated.h already included, missing '#pragma once' in PUCGameModeBase.h"
+#endif
+#define FPSTANK_PUCGameModeBase_generated_h
+
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_SPARSE_DATA
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_ACCESSORS
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPUCGameModeBase(); \
+	friend struct Z_Construct_UClass_APUCGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(APUCGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/FPSTank"), NO_API) \
+	DECLARE_SERIALIZER(APUCGameModeBase)
+
+
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APUCGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APUCGameModeBase(APUCGameModeBase&&); \
+	NO_API APUCGameModeBase(const APUCGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APUCGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APUCGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APUCGameModeBase) \
+	NO_API virtual ~APUCGameModeBase();
+
+
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_12_PROLOG
+#define FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_SPARSE_DATA \
+	FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_ACCESSORS \
+	FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FPSTANK_API UClass* StaticClass<class APUCGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_USER_Desktop_UEFPSClass_FPSTank_FPSTank_Source_FPSTank_Actors_GameMode_PUCGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ FPSTank/Source/FPSTank/Actors/GameMode/PUCGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PUCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTANK_API APUCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
